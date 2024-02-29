343b96140a68951c0ed4f0268f57dcc6c5a72f31
Keshav Kartik Sharma <keshav4762@gmail.com>
2024-02-29 14:45:24 +0530

    Desktop completed

--- src/app/Component/Header/Desktop.js
+++ src/app/Component/Header/Desktop.js
@@ -4,6 +4,9 @@ import React from "react";
 import { SiFlipkart } from "react-icons/si";
 import { CiSearch } from "react-icons/ci";
 import { FaRegUserCircle } from "react-icons/fa";
+import { IoCartOutline } from "react-icons/io5";
+import { CiShop } from "react-icons/ci";
+import { HiOutlineDotsVertical } from "react-icons/hi";
 
 
 
@@ -17,21 +20,36 @@ function Desktop() {
         </div>
 
         <div className="search flex flex-row items-center w-3/6">
-          <div className="search-icon cursor-pointer"> <CiSearch className="h-6 w-6" /> </div>
-          <input type="text" placeholder="Search for product, brands and more" className="search-bar border-solid border-2  rounded-md bg-sky-50 w-full p-2" />
+          <div className="search-icon cursor-pointer">
+            {" "}
+            <CiSearch className="h-6 w-6" />{" "}
+          </div>
+          <input
+            type="text"
+            placeholder="Search for product, brands and more"
+            className="search-bar border-solid border-2  rounded-md bg-sky-50 w-full p-2"
+          />
         </div>
 
         <div className="login flex flex-row items-center cursor-pointer hover:bg-sky-700">
-          <div className="user-icon"> <FaRegUserCircle className="h-7 w-7 mr-2" /> </div>
+          <div className="user-icon">
+            {" "}
+            <FaRegUserCircle className="h-7 w-7 mr-2" />{" "}
+          </div>
           <div className="login-text">Login</div>
         </div>
 
-        <div className="cart-seller flex flex-row">
-          <div className="cart">cart</div>
-          <div className="seller">seller</div>
+        <div className="flex flex-row items-center cart cursor-pointer">
+          <IoCartOutline className="h-7 w-7 mr-2" />
+          <div>Cart</div>
         </div>
 
-        <div className="three-dots">3 dots</div>
+        <div className="seller flex flex-row items-center cursor-pointer">
+        <CiShop className="h-7 w-7 mr-2" />
+        <div>Seller</div>
+        </div>
+
+        <div className="three-dots flex items-center cursor-pointer"> <HiOutlineDotsVertical /> </div>
       </header>
     </div>
   );
