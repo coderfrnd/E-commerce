30516b763672533a6957570fd5a228e927c861e2
Keshav Kartik Sharma <keshav4762@gmail.com>
2024-03-22 21:08:56 +0530

    fireboult updated

--- src/app/fireboult/page.jsx
+++ src/app/fireboult/page.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { SlGraph } from "react-icons/sl";
+import { IoIosStar, IoIosStarOutline } from "react-icons/io";
+import { IoMdStarOutline } from "react-icons/io";
 
 
 function page() {
@@ -65,6 +67,44 @@ function page() {
         <div className="flex justify-between p-4 border-b">
           <div>Display Size: 1.83 inch </div>
           <div className="text-slate-500">3 more</div>
+        </div>
+      </div>
+
+      <div className="mx-2 my-4">
+        <p className="text-sm"><b>Fire-Boult</b> Hurricane 1.33 inches Curved Glass Display with 360 Health Training1 100+ Sports Modes Smartwatch (Gray Strap, Free Size) </p>
+
+        <div className="flex items-center my-2">
+          <IoIosStar color="green" />
+          <IoIosStar color="green" />
+          <IoIosStar color="green" />
+          <IoIosStar color="green" />
+          <IoIosStarOutline color="green" />
+          <p className="text-blue-600 text-xs ml-2">2,150,384 ratings</p>
+        </div>
+
+        <div className="flex">
+          <h2 className="m font-semibold text-green-700 text-xl">35% off</h2>
+          <h2 className="m-1 font-semibold text-slate-400 line-through text-xl">8999</h2>
+          <h2 className="m-1 font-semibold text-slate-700 text-xl">₹1,999</h2>
+        </div>
+
+        <div>
+          <p className="flex text-sm items-center text-green-700">Free delivery by <p className="ml-1 text-slate-800">25th March</p></p>
+        </div>
+
+      </div>
+
+      <div className="mx-2 my-4">
+        <div className="flex">
+          <div>Icon</div>
+
+          <div>
+            <div className="flex">
+              <div className="text-green-600 flex">FREE Delivery <p className="text-slate-400 line-through mx-2">₹40</p></div>
+              <div className="">Delivery by:</div>
+            </div>
+              <div>25th Mar, Monday</div>
+          </div>
         </div>
       </div>
 
